3fdd1871cd2838e3660dc7392952174636d4e454
David <chuckbucker94@gmail.com>
2023-03-12 13:31:44 +0300

    Info display in Logcat. onClick method implemented in both ways

--- app/src/main/java/com/example/prac2_r/MainActivity.java
+++ app/src/main/java/com/example/prac2_r/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.prac2_r;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,20 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1 = findViewById(R.id.button_login);
-        button2 = findViewById(R.id.button_signup);
+        //button2 = findViewById(R.id.button_signup);
         field1 = findViewById(R.id.editTextTextEmailAddress);
         field2 = findViewById(R.id.editTextTextPassword);
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                Log.i(this.getClass().getSimpleName(), "Info");
 
             }
         });
 
     }
+    public void onClickLogIn(View v) {
+        Log.i(this.getClass().getSimpleName(), "Info");
+    }
 
-    private void onClickSignUp(View v){
+    public void onClickSignUp(View v) {
 
     }
+
 }
